df57b46ac523852c4e119620499f8bb94baab20a
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-12-27 15:30:39 +0500

    Initial Commit

---
+++ UcellTimeline/UcellTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  UcellTimelineApp.swift
+//  UcellTimeline
+//
+//  Created by A'zamjon Abdumuxtorov on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UcellTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
